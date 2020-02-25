ff36c08001d56b74d648d8419369b0f50a5f7f84
Ariane4 <abischoff48@gmailcom>
2020-02-25 10:53:07 +0100

    Start

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+function CalculDiff (){
+    let Differenz = document.getElementById("age1").value - document.getElementById("age2").value;
+    document.getElementById("difference").innerHTML = Differenz; 
+}
